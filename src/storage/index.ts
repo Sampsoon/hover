7fe761e5d40d384351a62bab9f637f4bd674cac3
sampsoon <28665112+Sampsoon@users.noreply.github.com>
2025-11-22 18:14:08 -0500

    Rename apiKeys.ts to getAPIKeyConfig.ts

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,3 +1,3 @@
-export { getAPIKeyConfig, DEFAULT_MODEL, OPEN_ROUTER_API_URL } from './apiKeys';
+export { getAPIKeyConfig, DEFAULT_MODEL, OPEN_ROUTER_API_URL } from './getAPIKeyConfig';
 export { storage } from './storage';
 export { APIProvider } from './types';
